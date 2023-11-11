b33017e154b768a4b65ed69481e9dc62f4cb4a40
David Thiel <Dudavik356@yandex.ru>
2023-11-11 16:05:34 +0600

    add calculate form

---
+++ spurs/mainpage/views.py
@@ -0,0 +1,27 @@
+import django.shortcuts
+import requests
+
+from . import forms
+
+def index(request):
+    template = "mainpage/index.html"
+    calculate_form = forms.CalculateForm(request.POST or None)
+    calculate_change_form = forms.CalculateChangeForm(request.POST or None)
+    if calculate_form.is_valid() and calculate_change_form.is_valid():
+        calculate_data = calculate_form.cleaned_data
+        calculate_change_data = calculate_change_form.cleaned_data
+        print(calculate_change_data, calculate_data)
+    
+    calculate_form_fields = calculate_form.visible_fields()
+    context = {
+        "fields_first_col": calculate_form_fields[::2],
+        "fields_second_col": calculate_form_fields[1::2],
+        "calculate_change_form": calculate_change_form,
+    }
+        
+    return django.shortcuts.render(
+        request,
+        template_name=template,
+        context=context
+    )
+    
